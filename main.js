a55642e530b24d40d28c32bae5d6e8add8a797bb
thandiwemasuku <50559699+thandiwemasuku@users.noreply.github.com>
2020-02-03 13:29:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+
+    });
+	});
